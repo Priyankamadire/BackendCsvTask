005ce65d6ed17e6d999add5174ce2e604fcd2dae
priyanka <madirepriyanka01@gmail.com>
2024-05-26 22:09:54 +0530

    MyBackendCsvTask

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// server.js
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const uploadRoute = require("./routes/Upload");
-const gettimedetailRouter = require("./routes/gettimedetail"); // Import the new router
+const gettimedetailRouter = require("./routes/gettimedetail");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
@@ -13,7 +12,7 @@ console.log(process.env.PORT);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(require("./routes/Upload"));
-// app.use("/api", gettimedetailRouter); // Use the new router for asset-wise balance
+
 app.use(require("./routes/gettimedetail"));
 require("./connection");
 app.get("/", (req, res) => {
